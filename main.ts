9be9aa7960e8270b9deb4c12acc1817f30f77860
318511 <91246969+318511@users.noreply.github.com>
2021-11-04 16:15:36 +0800

    更新 main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,18 @@
+function 旋轉 (around: number) {
+    if (around < 4) {
+        a = around
+        b = 1
+    } else if (around >= 4 && around < 6) {
+        a = 3
+        b = around - 2
+    } else if (around >= 6 && around < 8) {
+        a = 8 - around
+        b = 3
+    } else if (around >= 8) {
+        a = 1
+        b = 10 - around
+    }
+}
 function 燈數 (num: number) {
     if (num < 6) {
         x = num - 1
@@ -15,26 +30,47 @@ function 燈數 (num: number) {
 }
 input.onButtonPressed(Button.A, function () {
     while (!(input.buttonIsPressed(Button.B))) {
-        for (let index = 0; index <= 16; index++) {
+        for (let index = 0; index <= 15; index++) {
             N = index + 1
-            燈數(index)
+            燈數(N)
             led.plot(x, y)
-            燈數(index - 1)
-            led.unplot(x, y)
             basic.pause(100)
+            led.unplot(x, y)
+            燈數(N - 1)
         }
     }
 })
-input.onButtonPressed(Button.B, function () {
-    for (let index = 0; index <= 16; index++) {
-        N = 0 + 1
+input.onButtonPressed(Button.AB, function () {
+    for (let index = 0; index <= 15; index++) {
+        N = index + 1
+        燈數(N)
         led.plot(x, y)
-        N = 0
-        led.unplot(x, y)
         basic.pause(100)
     }
+    for (let index2 = 0; index2 <= 7; index2++) {
+        N2 = index2 + 1
+        旋轉(N2)
+        led.plot(a, b)
+        basic.pause(100)
+    }
+    led.plot(2, 2)
 })
+input.onButtonPressed(Button.B, function () {
+    while (!(input.buttonIsPressed(Button.A))) {
+        for (let index = 0; index <= 15; index++) {
+            N = 16 - index
+            燈數(N)
+            led.plot(x, y)
+            basic.pause(100)
+            led.unplot(x, y)
+            燈數(N - 1)
+        }
+    }
+})
+let N2 = 0
 let N = 0
+let b = 0
+let a = 0
 let y = 0
 let x = 0
 x = 0
